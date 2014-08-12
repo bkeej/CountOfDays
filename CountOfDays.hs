989b1ed548dd5b50e0e13c66484517e8bdd8f8ca
balamkej <balamkej@gmail.com>
2014-08-12 16:04:49 -0400

    Commented main block

--- CountOfDays.hs
+++ CountOfDays.hs
@@ -6,6 +6,7 @@ import Data.Time.Calendar
 import Data.List
 import MayanCal
 
+--The script takes one command line argument, a day formatted like "21 Dec 2012", and returns the Mayan long count, haab, and cholqij dates as a string.
 main = do
     args <- getArgs
     --date <- getCurrentTime >>= return . countOfDays . modJDToJD . toModifiedJulianDay . utctDay
